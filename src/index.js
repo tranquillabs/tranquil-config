ff051a1bb53290746a304c783ae9870479327892
Kabootit <kabootit@users.noreply.github.com>
2026-06-18 22:26:29 -0400

    feat: activate tree-view defaults to hide system and gitignored files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-module.exports = require('./generated/index');
+module.exports = {
+  activate() {
+    atom.config.setDefaults('tree-view', {
+      hideIgnoredNames: true,
+      hideVcsIgnoredFiles: true,
+    });
+  }
+};
